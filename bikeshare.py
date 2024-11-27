c1e7189873bc49616cf98c12cea4ffae9ce4b472
TuanTran <tung.tuantran@coxautoinc.com>
2024-11-28 06:53:04 +0700

    update comment for bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -56,7 +56,7 @@ def get_filters():
 
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Loads all data for the specified city and filters by month and day.
 
     Args:
         (str) city - name of the city to analyze
